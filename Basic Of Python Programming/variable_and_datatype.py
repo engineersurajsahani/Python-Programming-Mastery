23c9fe402115818d810ad667a0265dae8ee83f4b
surajsahani9321 <radhashyam9321@gmail.com>
2025-06-09 13:16:30 +0530

    variable & datatype

---
+++ Basic Of Python Programming/variable_and_datatype.py
@@ -0,0 +1,32 @@
+a=5 # 101 a = 5 
+
+b=6.6 # 102 b = 6.6 
+
+c='m'
+
+d="Suraj Sahani"
+
+print(a)
+print(type(a))
+print(b)
+print(type(b))
+print(c)
+print(type(c))
+print(d)
+print(type(d))
+
+print(a,b,c,d)
+print(type(a),type(b),type(c),type(d))
+
+print("Value of a :- ",a)
+print("Value of b :- ",b)
+print("Value of c :- ",c)
+print("Value of d :- ",d)
+
+# Variable :- It is like a conatiner in which we can 
+# store some data.
+
+# It is the name of the memory location.
+
+# Data Type :- It define the type of the value 
+# which are going to be store in the variable.
