b6f5b1db3b536bf3ba904554cf21f7aa8a8fc3dd
surajsahani9321 <radhashyam9321@gmail.com>
2025-06-13 14:38:24 +0530

    loop statements

---
+++ Basic Of Python Programming/loop_statements.py
@@ -0,0 +1,26 @@
+# Loop Statements 
+
+# for loop 
+# while loop 
+
+for i in range(1,11) :
+    print("HI")
+
+i=1 
+
+while i<11 :
+    print("Hello")
+    i+=1
+
+# 2 * 1 = 2 
+# 2 * 2 = 4 
+#     i   2*i 
+
+for i in range(1,11):
+    print("2 * ",i," = ",2*i)
+
+i=1 
+
+while i<11 :
+    print("3 * ",i," = ",3*i)
+    i+=1
