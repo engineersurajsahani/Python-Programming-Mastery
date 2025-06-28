1a19719c8759809768a6767c26dd57ae7fcdf5a5
surajsahani9321 <radhashyam9321@gmail.com>
2025-06-28 09:51:12 +0530

    set program

---
+++ Data Structure/set_program.py
@@ -0,0 +1,22 @@
+# Set :-  
+
+# (1) It store only unique elements
+# (2) It does not maintain insertion order
+
+s=set()
+
+s.add(10)
+s.add(44)
+s.add(88)
+s.add(44)
+
+print(s)
+
+l=[1,2,3,4,5,6,7,8,9,3,4,5,6]
+print(l)
+
+l=set(l)
+
+l=list(l)
+
+print(l)
