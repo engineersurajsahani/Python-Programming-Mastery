0c6bee408bf4146094fe8d1ad1d6d379926645d0
surajsahani9321 <radhashyam9321@gmail.com>
2025-09-07 14:56:14 +0530

    method overloading and compile time polymorphism

---
+++ Object Oriented Programming/method_overloading.py
@@ -0,0 +1,13 @@
+class A:
+
+    def add(self,a,b):
+        print(a+b)
+
+    def add(self,a,b,c):
+        print(a+b+c)
+
+    def add(self,a,b,c,d):
+        print(a+b+c+d)
+
+a=A()
+a.add(10,20,30,40)
